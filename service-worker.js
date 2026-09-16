0dc0cb269197f6cf5f46f24d0aa075845bb803b4
rulymauzi-byte <rulymauzi@gmail.com>
2026-09-16 16:33:08 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = 'himnario-bit-yik-2026-09-16-v5';
+const CACHE_NAME = 'himnario-bit-yik-2026-09-16-v6';
 const APP_SHELL = [
+  './index.html',
   './manifest.webmanifest',
   './icons/icon-144.png',
   './icons/icon-192.png',
